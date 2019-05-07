23f0f9ed1527f1525c94e6c15a4b8792a30da3c0
ScottyOBrien <obrie023@rangers.uwp.edu>
2019-05-06 21:30:13 -0500

    Initial commit

---
+++ src/PlagiarismChecker.java
@@ -0,0 +1,25 @@
+/**
+ * @author Scott O'Brien
+ * due: 05092019
+ *
+ * PlagiarismChecker is a programm that compares two given programs and compares the code to see how similar they are.
+ * It compares subsequences and keeps track of the longest subsequence and gives the two programs a "plagerism score".
+ *
+ * KNOWN BUGS:
+ */
+public class PlagiarismChecker {
+
+    public static void main(String[] args) {
+
+    }
+
+    public double plagiarismScore(String filename1, String filename2) {
+
+        return 0.0;
+    }
+
+    public void plagiarismChecker (String[] filenames, double threshold) {
+
+    }
+
+}
